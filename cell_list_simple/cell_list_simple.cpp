5439c447662d41e1f76806485bee7a8025c76753
Eric Medwedeff <emedwede@uci.edu>
2021-01-18 12:28:54 -0800

    Added the basic class structure

--- cell_list_simple/cell_list_simple.cpp
+++ cell_list_simple/cell_list_simple.cpp
@@ -1,5 +1,32 @@
 #include<iostream>
 
+#include <Cabana_Core.hpp>
+#include <Kokkos_Core.hpp>
+
+//Linked Cell List
+
+namespace Cajete {
+
+template<class DeviceType>
+class LLCellList {
+    public:
+        using device_type = DeviceType;
+        using memory_space = typename device_type::memory_space;
+        using execution_space = typename device_type::execution_space;
+        using size_type = typename memory_space::size_type;
+        using OffsetView = Kokkos::View<size_type*, device_type>;
+
+};
+
+}
+
 int main() {
+    using MemorySpace = Kokkos::DefaultExecutionSpace::memory_space;
+    using ExecutionSpace = Kokkos::DefaultExecutionSpace;
+    using DeviceType = Kokkos::Device<ExecutionSpace, MemorySpace>;
+
+    using NodeTypes = Cabana::MemberTypes<double[3], size_t>;
+    
+    Cajete::LLCellList<DeviceType> cell_list;
     std::cout << "Hello simple\n";
 }
