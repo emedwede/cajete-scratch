a36c10d196ade8246f2a64e0bfec3f3235a71990
Eric Medwedeff <emedwede@uci.edu>
2021-01-20 16:06:57 -0800

    Updated grid to find correct indicies and cardinal index

--- cell_list_simple/grid.hpp
+++ cell_list_simple/grid.hpp
@@ -32,21 +32,24 @@ class BrickGrid2D {
         //convenience we do
         BrickGrid2D(const Real min_x, const Real min_y, 
                   const Real max_x, const Real max_y,
-                  const Real dx, const Real dy,
-                  const Dims num_r)
+                  const Real delta_x, const Real delta_y)
             : _min_x(min_x)
             , _min_y(min_y)
             , _max_x(max_x)
             , _max_y(max_y)
-            , _dx(dx)
-            , _dy(dy)
-            , _num_r(num_r)
-            , _num_ee(num_r)
-            , _num_eo(num_r+1)
         {
-            //do more stuff here
-            //later we need to add a check that num_r > 1
+            //The grid construction is smart in the sense that it will
+            //make the grid cells a bit larger so that we can have our
+            //desired grid size
+            _num_ee = cartesianCellsBetween(max_x, min_x, 1.0/delta_x);
+            _num_eo = _num_ee+1;
+            _num_r  = cartesianCellsBetween(max_x, min_x, 1.0/delta_y);
+            _dx = (max_x - min_x)/_num_ee;
+            _dy = (max_y - min_y)/_num_r;
+            _rdx = 1.0/_dx;
+            _rdy = 1.0/_dy;
         }
+
         KOKKOS_INLINE_FUNCTION
         std::size_t totalNumCells() const {
             //something like this, in cartesian it's nx*ny*nz
@@ -57,23 +60,38 @@ class BrickGrid2D {
                 return (_num_r/2)*_num_ee+(_num_r/2)*_num_eo; 
             }
         }
-       
-        //TODO: Finish and correct locate point
+      
+        // Return the cardianal index of an ij cell index
         KOKKOS_INLINE_FUNCTION
-        void locatePoint(const Real xp, const Real yp, int& ic, int& jc) const {
-            //we first need to determine the row then we can locate the point
-            jc = floor((yp - _min_y) * 1/_dy);
-            if(jc%2) { //ODD
-                ic = floor((xp - _min_x) / _dx);
+        int cardinalCellIndex(const int i, const int j) const {
+            if(j%2) { 
+                return ((j-1)/2)*_num_ee+((j-1)/2)*_num_eo+_num_ee+i;
             } else { //EVEN
-                if(xp < _dx/2) {
-                    ic = 0;
-                } else if (xp > _dx/2) {
-                    ic = _num_ee - 1;
-                }
+                return (j/2)*_num_ee+(j/2)*_num_eo+i;
             }
         }
 
+        KOKKOS_INLINE_FUNCTION
+        void locatePoint(const Real xp, const Real yp, int& ic, int& jc) const {
+            //we first need to determine the row then we can locate the point
+            jc = floor((yp - _min_y) / _dy);
+            if(jc%2) { //ODD
+                if(xp < _dx/2) {
+                    ic = 0;
+                } else if (xp > _max_x - _dx/2) {
+                    ic = _num_eo - 1;
+                } else {
+                    ic = floor((xp - (_min_x + _dx/2))/_dx)+1;
+                }
+            } else { //EVEN
+                ic = floor((xp - _min_x) / _dx);
+            }
+        }
+
+        KOKKOS_INLINE_FUNCTION
+        int cartesianCellsBetween(const Real max, const Real min, const Real rdelta) const {
+            return floor((max-min)*rdelta);
+        }
         //size_t compute_num_2D_zones(); <==> same as totalNumCells() in 2D
         //size_t compute_num_1D_zones();
         //size_t compute_num_0D_zones();
