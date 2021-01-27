e969837d18208c883f4b53b6b9a4c164390cb0db
Eric Medwedeff <emedwede@uci.edu>
2021-01-27 14:43:53 -0800

    Fixed some compiler paths in the scripts for easy blazar compatibility

--- scripts/construct-cpu.sh
+++ scripts/construct-cpu.sh
@@ -5,7 +5,8 @@ mkdir cpu-build
 
 cd cpu-build
 
-cmake -DCMAKE_PREFIX_PATH="/home/erock/ExtraLibs/CajeteDepend/Cabana/cpu-build/install" \
+#Same path as on blazar to make life easier
+cmake -DCMAKE_PREFIX_PATH="~/ExtraLibs/CajeteDepend/Cabana/cpu-build/install" \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 
 make -j4
--- scripts/construct-gpu.sh
+++ scripts/construct-gpu.sh
@@ -5,9 +5,11 @@ rm -r -f gpu-build
 mkdir gpu-build
 
 cd gpu-build
-
-cmake -DCMAKE_PREFIX_PATH="/home/erock/ExtraLibs/CajeteDepend/Cabana/gpu-build/install" \
-    -DCMAKE_CXX_COMPILER="/home/erock/ExtraLibs/CajeteDepend/kokkos/bin/nvcc_wrapper" \
+HOME_PATH=~/
+COMPILER_PATH=$HOME_PATH"ExtraLibs/CajeteDepend/kokkos/bin/nvcc_wrapper"
+echo $COMPILER_PATH
+cmake -DCMAKE_PREFIX_PATH="~/ExtraLibs/CajeteDepend/Cabana/gpu-build/install" \
+    -DCMAKE_CXX_COMPILER=$COMPILER_PATH \
     -DCMAKE_CXX_EXTENSIONS=OFF \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 
