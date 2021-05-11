d2825369c60a125219024de3aec12f68d4845ab4
Eric Medwedeff <emedwede@uci.edu>
2021-05-11 15:46:32 -0700

    Modified virtual example

--- examples/virtual.cpp
+++ examples/virtual.cpp
@@ -2,6 +2,9 @@
 
 class ResourceBase {
     public:
+        
+        Kokkos::View<int*> data;
+
         KOKKOS_FUNCTION
         int allocate(int n) const {
             return do_allocate(n);
@@ -35,6 +38,9 @@ class ResourceBase {
 
         KOKKOS_FUNCTION
         virtual int do_at(int n) const = 0;
+    
+    protected:
+        int value;
 };
 
 class ChildResource : public ResourceBase {
@@ -43,10 +49,11 @@ class ChildResource : public ResourceBase {
         ChildResource(int c) {
             printf("Constructed child with value %d\n", c);
         }
+       
+        int member;
         
     private:
-        Kokkos::View<int*> data;
-        
+                
         KOKKOS_FUNCTION
         virtual int do_at(int n) const override {
             return data(n);
@@ -68,40 +75,41 @@ class ChildResource : public ResourceBase {
         }
 };
 
-class Foo {
-    protected:
-        int val;
+struct Base {
 
-    public:
-        KOKKOS_FUNCTION
-        Foo() {val = 0;}
+    KOKKOS_FUNCTION
+    Base() { printf("Base Constructor\n"); }
 
-        KOKKOS_FUNCTION
-        virtual int value() const = 0;
-        //virtual int value() {return 0;}
+    virtual void build_data(int c) = 0;
 
-        KOKKOS_FUNCTION
-        virtual ~Foo() {}
-
+    KOKKOS_FUNCTION
+    virtual void set_value(int input) = 0;
+    KOKKOS_FUNCTION
+    virtual ~Base() { printf("Base Destructor\n"); }
 };
 
-class Foo_1 : public Foo {
-    public:
-        KOKKOS_FUNCTION
-        Foo_1() { val = 1; }
+struct Child : Base {
+    
+    Kokkos::View<int*> data;
+    int value;
 
-        KOKKOS_FUNCTION
-        virtual int value() const override { return val; }
+    KOKKOS_FUNCTION
+    Child() { 
+        printf("Child contructor\n");
+    }
+    
+    virtual void build_data(int c) override {
+        data = Kokkos::View<int*>("name", c);
+    }
+
+    KOKKOS_FUNCTION
+    virtual void set_value(int input) override {
+        value = input;
+    } 
 };
 
-class Foo_2 : public Foo {
-    public:
-        KOKKOS_FUNCTION
-        Foo_2() { val = 2; }
-
-        KOKKOS_FUNCTION
-        int value() const { return val; }
-};
+//There is a difference between virtual functions that control execution on the host
+//and those that are used in kernel!
 
 int main (int argc, char* argv[]) {
     Kokkos::ScopeGuard scope_guard(argc, argv);
@@ -112,40 +120,57 @@ int main (int argc, char* argv[]) {
     using DeviceType = Kokkos::HostSpace;
     #endif
 
-    //ResourceBase* deviceChildResource = 
-    //    (ResourceBase*)Kokkos::kokkos_malloc<DeviceType>(sizeof(ChildResource));
-   
-    ChildResource* deviceChildResource = 
-        (ChildResource*)Kokkos::kokkos_malloc<DeviceType>(sizeof(ChildResource));
-    
- 
-    Foo* f_1 = (Foo*)Kokkos::kokkos_malloc<DeviceType>(sizeof(Foo_1));
-    Kokkos::parallel_for("Create", 1, KOKKOS_LAMBDA(const int&) {
-        new ((Foo_1*)f_1) Foo_1();
-        new ((ChildResource*)deviceChildResource) ChildResource(10);
-    });
+    ResourceBase* deviceChildResource = 
+        (ResourceBase*)Kokkos::kokkos_malloc<DeviceType>(sizeof(ChildResource));
+  
+    Child* deviceChild = (Child*)Kokkos::kokkos_malloc<DeviceType>(sizeof(Child));
+    //ChildResource* deviceChildResource = 
+    //    (ChildResource*)Kokkos::kokkos_malloc<DeviceType>(sizeof(ChildResource));
+    Child* hostChild = (Child*)Kokkos::kokkos_malloc<Kokkos::HostSpace>(sizeof(Child));
 
-    deviceChildResource->set_data(10);
+    Kokkos::parallel_for("Create", 1, KOKKOS_LAMBDA(const int&) {
+        new ((ChildResource*)deviceChildResource) ChildResource(10);
+        new ((Child*)deviceChild) Child();
+    });
     
+    int* ptr = (int*) Kokkos::kokkos_malloc<DeviceType>(sizeof(int)*3);
+    Kokkos::View<int*, DeviceType> test(ptr, 3);
+    for(auto i = 0; i < 3; i++) {
+        test(i) = i+3;
+        std::cout << test(i) << " ";
+    } std::cout << std::endl;
+
+    Kokkos::View<Child*> deviceChild_v(deviceChild, 1);
+    Kokkos::View<Child*, Kokkos::HostSpace> hostChild_v(hostChild, 1);
+    
+    
+    Kokkos::deep_copy(hostChild_v, deviceChild_v);
+    //hostChild_v(0).build_data(10);
+    Kokkos::deep_copy(deviceChild_v, hostChild_v);
+
+    //deviceChildResource->data = Kokkos::View<int*>("name", 10);
+    
+    ((ChildResource*)deviceChildResource)->member = 2;
+    //deviceChildResource->set_data(10);
+    //deviceChildResource->allocate(1); 
     Kokkos::parallel_for("Virtual Dispatch", 10, KOKKOS_LAMBDA(const int i) {
-        //int a = f_1->value();
-        //printf("Value: %d\n", a);
         //deviceChildResource->allocate(1);
         //deviceChildResource->insert(i, i*2);
-
     }); 
-   
+    Kokkos::fence();
+    //deviceChild->build_data(10);
+    
     for(auto i = 0; i < 10; i++) {
         //std::cout << deviceChid
     }
     Kokkos::parallel_for("Destroy", 1, KOKKOS_LAMBDA(const int&) {
-        f_1->~Foo();
-        deviceChildResource->~ChildResource();
-        //deviceChildResource->~ResourceBase();
+        //deviceChildResource->~ChildResource();
+        deviceChildResource->~ResourceBase();
+        deviceChild->~Child();
     });
 
-    Kokkos::kokkos_free(f_1);
     Kokkos::kokkos_free(deviceChildResource);
-
+    Kokkos::kokkos_free(deviceChild);
+    Kokkos::kokkos_free(hostChild);
     return 0;
 }
